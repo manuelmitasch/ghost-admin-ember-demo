6931e5e444818f67b3af274d7205d752a29067cf
Manuel Mitasch <manuel@cms.mine.nu>
2014-02-17 15:38:03 +0100

    Move word count from controller to a helper

--- app/controllers/editor.js
+++
@@ -1,7 +0,0 @@
-import count from "ghost/utils/word-counter";
-
-export default Ember.ObjectController.extend({
-  wordCount: function(){
-    return count(this.get('markdown'));
-  }.property("markdown"),
-});
