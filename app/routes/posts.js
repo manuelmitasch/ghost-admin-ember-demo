0625d4f60f52f40a53eeb21920b65c4541c7e6a2
Manuel Mitasch <manuel@cms.mine.nu>
2014-02-17 17:00:57 +0100

    item-view now triggers an action instead of transitioning directly

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -5,6 +5,12 @@ export default Ember.Route.extend({
 
   model: function() {
     return ajax("/ghost/api/v0.1/posts");
+  },
+
+  actions: {
+    openEditor: function(post) {
+      this.transitionTo('editor', post);
+    }
   }
 });
 
--- app/views/post-item-view.js
+++ app/views/post-item-view.js
@@ -2,6 +2,6 @@ import itemView from 'ghost/views/item-view';
 
 export default itemView.extend({
   openEditor: function() {
-    this.get('controller').transitionToRoute("editor", this.get('post'));
+    this.get('controller').send('openEditor', this.get('post'));  // send action to handle transition to editor route
   }.on("doubleClick")
 });
